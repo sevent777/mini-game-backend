b94e4d27ef3a1bc4450f851bf3d889bcaeb8a301
Wukun Cai <wukun.cai@shopee.com>
2023-05-23 14:38:53 +0800

    feat: rm useless code

--- src/index.ts
+++ src/index.ts
@@ -1,40 +1,23 @@
-import Koa from "koa";
-import Router from "koa-router";
-import logger from "koa-logger";
-import bodyParser from "koa-bodyparser";
-import fs from "fs";
-import path from "path";
-import { init as initDB, Counter } from "./db";
+import fs from 'fs';
+import Koa from 'koa';
+import bodyParser from 'koa-bodyparser';
+import logger from 'koa-logger';
+import Router from 'koa-router';
+import path from 'path';
+
+import { Counter, init as initDB } from './db';
 
 const router = new Router();
 
-const homePage = fs.readFileSync(path.join(__dirname, "../index.html"), "utf-8");
+const homePage = fs.readFileSync(path.join(__dirname, '../index.html'), 'utf-8');
 
 // 首页
-router.get("/", async (ctx) => {
+router.get('/', async (ctx) => {
   ctx.body = homePage;
 });
 
-// 更新计数
-router.post("/api/count", async (ctx) => {
-  const { request } = ctx;
-  const { action } = (request.body) as any;
-  if (action === "inc") {
-    await Counter.create();
-  } else if (action === "clear") {
-    await Counter.destroy({
-      truncate: true,
-    });
-  }
-
-  ctx.body = {
-    code: 0,
-    data: await Counter.count(),
-  };
-});
-
 // 获取计数
-router.get("/api/count", async (ctx) => {
+router.get('/api/count', async (ctx) => {
   const result = await Counter.count();
 
   ctx.body = {
@@ -44,24 +27,20 @@ router.get("/api/count", async (ctx) => {
 });
 
 // 小程序调用，获取微信 Open ID
-router.get("/api/wx_openid", async (ctx) => {
-  if (ctx.request.headers["x-wx-source"]) {
-    ctx.body = ctx.request.headers["x-wx-openid"];
+router.get('/api/wx_openid', async (ctx) => {
+  if (ctx.request.headers['x-wx-source']) {
+    ctx.body = ctx.request.headers['x-wx-openid'];
   }
 });
 
 const app = new Koa();
-app
-  .use(logger())
-  .use(bodyParser())
-  .use(router.routes())
-  .use(router.allowedMethods());
+app.use(logger()).use(bodyParser()).use(router.routes()).use(router.allowedMethods());
 
 const port = process.env.PORT || 80;
 async function bootstrap() {
   await initDB();
   app.listen(port, () => {
-    console.log("启动成功", port);
+    console.log('启动成功', port);
   });
 }
 bootstrap();
