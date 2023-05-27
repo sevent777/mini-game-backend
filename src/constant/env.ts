5a04cc315f689854d77291d508fb803176826530
Wukun Cai <wukun.cai@shopee.com>
2023-05-27 21:22:37 +0800

    feat: debug live env

--- src/constant/env.ts
+++ src/constant/env.ts
@@ -7,6 +7,7 @@ if (process.env.NODE_ENV === 'development') {
 const port = process.env.PORT || 80;
 
 const { MYSQL_USERNAME, MYSQL_PASSWORD, MYSQL_ADDRESS = '' } = process.env;
+console.log('object :>> ', MYSQL_USERNAME, MYSQL_PASSWORD, MYSQL_ADDRESS, port);
 const [MYSQL_HOST, MYSQL_PORT] = MYSQL_ADDRESS.split(':');
 
 export { MYSQL_HOST, MYSQL_PASSWORD, MYSQL_PORT, MYSQL_USERNAME, port };
